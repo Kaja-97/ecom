67e041a7653acee0adcf1c35f3a7bdec50cca8bf
Kajanan <110709649+Kaja-97@users.noreply.github.com>
2022-11-24 14:00:33 +0530

    Update settings.py

--- E_commerce/settings.py
+++ E_commerce/settings.py
@@ -126,15 +126,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-# STATIC_URL = 'static/'
-# MEDIA_URL = '/images/'
 MEDIA_ROOT= os.path.join(BASE_DIR,'staticfiles')
 
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'static'),
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')
                  ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
+
+
+STATIC_URL = 'static/'
+MEDIA_URL = '/images/'
 
 
 # Default primary key field type
