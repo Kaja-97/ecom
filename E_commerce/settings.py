3fa0e194195da82050e5cfa62a1f440a0fd28424
Kajanan <110709649+Kaja-97@users.noreply.github.com>
2022-11-24 13:45:06 +0530

    Update settings.py

--- E_commerce/settings.py
+++ E_commerce/settings.py
@@ -126,8 +126,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_URL = '/images/'
+# STATIC_URL = 'static/'
+# MEDIA_URL = '/images/'
 MEDIA_ROOT= os.path.join(BASE_DIR,'staticfiles')
 
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static'),
